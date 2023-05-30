0ffeee72940ee67ad421f4005f61faf78cb1316b
Tom Webster <tomwebster98@gmail.com>
2023-05-30 15:12:32 +0100

    Creates MATLAB/Simulink directory, adds Pacejka parameters script.

---
+++ Vehicle_Dynamics_Modelling_and_Simulation/MATLAB_Simulink_Models/Pacejka_lateral_parameters.m
@@ -0,0 +1,15 @@
+% Coefficients of the Pacejka Tyre Model - Determined from measured tyre
+% data and optimised to fit data appropriately.
+
+% Shape Factor (C)
+c = 1.5511;
+
+% Stiffness Factor (B)
+B = 0.1453;
+
+% Curvature Factor (E)
+E = -2.6166;
+
+% Peak Value
+a1 = -1e-5;
+a2 = 1.25;
