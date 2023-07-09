9cca839559a318abb71fbc89401c12d835a65acc
Tom Webster <tomwebster98@gmail.com>
2023-07-09 14:31:54 +0100

    Corrects rounding errors in formula vs vehicle comparisons

--- Vehicle_Dynamics_Modelling_and_Simulation/MATLAB_Simulink_Models/Lateral_Dynamics_Assignment/Formula_vs_Road_Vehicle_Comparisons.m
+++ Vehicle_Dynamics_Modelling_and_Simulation/MATLAB_Simulink_Models/Lateral_Dynamics_Assignment/Formula_vs_Road_Vehicle_Comparisons.m
@@ -18,22 +18,17 @@
 load("Formula_SteadyState_20s.mat")
 load("Road_Vehicle_SteadyState_40s.mat")
 %%
-plot(Formula_Steady_State_20s.SWA.Data, Formula_Steady_State_20s.latacc.Data);
+p1 = plot(Formula_Steady_State_20s.latacc.Data, Formula_Steady_State_20s.SWA.Data);
 hold on
-plot(Road_Vehicle_Steady_State_40s.SWA.Data, Road_Vehicle_Steady_State_40s.latacc.Data);
-xlabel("Steering Wheel Angle (deg)")
-ylabel("Lateral Acceleration m/s^2")
-title("LatAcc vs SWA Comparison for Formula vs Road Vehicle")
-legend(["Formula Vehicle", "Road Vehicle"])
-hold off
-
-plot(Formula_Steady_State_20s.latacc.Data, Formula_Steady_State_20s.SWA.Data);
-hold on
-plot(Road_Vehicle_Steady_State_40s.latacc.Data, Road_Vehicle_Steady_State_40s.SWA.Data);
+p2 = plot(Road_Vehicle_Steady_State_40s.latacc.Data, Road_Vehicle_Steady_State_40s.SWA.Data);
 ylabel("Steering Wheel Angle (deg)")
 xlabel("Lateral Acceleration m/s^2")
 title("SWA vs LatAcc Comparison for Formula vs Road Vehicle")
 legend(["Formula Vehicle", "Road Vehicle"])
+datatip(p1, 4, 8.7);
+datatip(p1, 2, 4.3);
+datatip(p2, 4, 23.5);
+datatip(p2, 2, 12.9);
 hold off
 %% 
 % Comparing these two vehicles and the change in lateral acceleration due to 
@@ -49,29 +44,24 @@ hold off
 % here.
 % 
 % We can observe from the graph that in the linear region (~2-4 ms^-2 of lateral 
-% acceleration) the formula vehicle shows a gradient of ~0.47 ms^-2 of lateral 
-% acceleration per degree of steering wheel input angle, while the road vehicle 
-% shows a much shallower response of ~0.19 ms^-2 / deg.
+% acceleration) the formula vehicle shows a gradient of ~5.34  deg / ms^-2, while 
+% the road vehicle shows a much shallower response of ~2.13 deg / ms^-2. The Formula 
+% vehicle response is more than double that of the road vehicle.
 % 
-% 
-% 
-% Considering the inverse, the road vehicle requires more steering wheel angle 
-% input from the driver to maintain a constant radius for a given velocity than 
-% the formula vehicle requires.
+% Note: The constant speed of 120km/h is a reltively low speed for a performance 
+% vehicle, wheras it is at the higher end of typical operating speeds for a road 
+% going vehicle.
 %% Calculating the understeer gradient of each vehicle.
-% Assuming the LatAcc vs SWA gradient to be 0.5ms^-2 / deg for the FV, therefore 
-% SWA vs LatAcc gradient is approximated as 2 deg / ms^-2.
-% 
-% Assuming the LatAcc vs SWA gradient to be 0.2ms^-2 / deg for the RV, therefore 
-% SWA vs LatAcc gradient is approcimated as 5 deg / ms^-2.
+% For the Formula Vehicle SWA gradient of 5.34 deg/ms^-2 and Road Vehicle SWA 
+% gradient 2.13 deg/ms^-2, we can determine the understeer gradient:
 % 
 % K_us = [(dSWA/dLatAcc) / SR] - [(L/V^2) .* 180/pi]
 
-Kus_FormulaV = (2 .* (1/10)) - ((3.5 / ((120/3.6)^2)) .* (180/pi))
-Kus_RoadV = (5 .* (1/20)) - ((2.8 / ((120/3.6)^2)) .* (180/pi))
+Kus_FormulaV = (2.13 .* (1/10)) - ((3.5 / ((120/3.6)^2)) .* (180/pi))
+Kus_RoadV = (5.34 .* (1/20)) - ((2.8 / ((120/3.6)^2)) .* (180/pi))
 %% 
-% With the Formula Vehicle having an understeer ratio of ~0.0195 and the Road 
-% Vehicle having an understeer ratio of ~0.1056, we can see that both vehicles 
+% With the Formula Vehicle having an understeer ratio of 0.0325 and the Road 
+% Vehicle having an understeer ratio of 0.1226, we can see that both vehicles 
 % are set up to have more understeer than oversteer. However, the formula vehicle 
 % is closer to a neutral setup than the road vehicle, since it has an understeer 
 % ratio ~5x less - in line with the epxectation that the road vehicle will need 
@@ -81,27 +71,34 @@ Kus_RoadV = (5 .* (1/20)) - ((2.8 / ((120/3.6)^2)) .* (180/pi))
 % input and maintain the path radius independent of the vehicle speed.
 %% Comparing Side Slip vs Lateral Acceleration for each vehicle.
 
-plot(Formula_Steady_State_20s.latacc.Data, Formula_Steady_State_20s.sideslip.Data);
+p3 = plot(Formula_Steady_State_20s.latacc.Data, Formula_Steady_State_20s.sideslip.Data);
 hold on
-plot(Road_Vehicle_Steady_State_40s.latacc.Data, Road_Vehicle_Steady_State_40s.sideslip.Data);
+p4 = plot(Road_Vehicle_Steady_State_40s.latacc.Data, Road_Vehicle_Steady_State_40s.sideslip.Data);
 ylabel("Side Slip (deg)")
 xlabel("Lateral Acceleration m/s^2")
 title("Side Slip vs LatAcc Comparison for Formula vs Road Vehicle")
 legend(["Formula Vehicle", "Road Vehicle"])
+datatip(p3, 4, -0.1);
+datatip(p3, 2, -0.06);
+datatip(p4, 4, -3);
+datatip(p4, 2, -1.65);
 hold off
 %% 
 % Comparing the trend of each line, we can clearly see the formula vehicle retains 
 % minimal side slip (magnitude) as the lateral acceleration increases, with a 
-% gradient of ~-0.1 deg / ms^-2. This means that the side slip increases in magnitude 
-% by 0.1 degree for each unit m/s^2 increase of lateral acceleration. In comparison, 
-% the road vehicle shows a steep increase in side slip angle magnitude for each 
-% unit lateral acceleration, with a gradient of ~-2.3 deg / ms^-2. Recalling that 
-% the vehicle sideslip angle is the angle offset between the direction of the 
-% wheel and the direction of motion of the vehicle, we expect this angle to be 
-% greater in an understeer scenario compared to a balanced or neutral vehicle 
+% gradient of ~-0.0345 deg / ms^-2. This means that the side slip increases in 
+% magnitude by 0.03 degrees for each unit m/s^2 increase of lateral acceleration. 
+% In comparison, the road vehicle shows a steep increase in side slip angle magnitude 
+% for each unit lateral acceleration, with a gradient of ~-0.6790 deg / ms^-2. 
+% Recalling that the vehicle sideslip angle is the angle offset between the direction 
+% of the wheel and the direction of motion of the vehicle, we expect this angle 
+% to be greater in an understeer scenario compared to a balanced or neutral vehicle 
 % setup, and so this outpur is as expected, since we already determined that the 
 % formula vehicle is extremely close to neutral, whereas the road vehicle has 
 % greater understeer ratio.
+% 
+% The road vehicle requires ~20 times more side slip to generate the same level 
+% of lateral acceleration.
 %% Step Steer Response
 %% Comparing Lateral Acceleration Overshoot
 %% 
@@ -149,6 +146,9 @@ LatAcc_Overshoot_RV = 100 .* ((max(Road_Vehicle_Step_Steer_10s.latacc.Data) - Ro
 % essentially no lateral acceleration overshoot (0.0005%) while the road vehicle 
 % overshoots by ~3% (2.9792%). This is expected, sine the performance and repsonsiveness 
 % of the formula vehicle should be greater than that of a road vehicle.
+% 
+% The formula vehicle is critically dampes, showing no overshoot in lateral 
+% acceleration.
 %% Comparing Yaw Rate Overshoot
 
 plot(Formula_Step_Steer_10s.yawrate.Time, Formula_Step_Steer_10s.yawrate.Data);
@@ -170,11 +170,11 @@ YawRate_Overshoot_RV = 100 .* ((max(Road_Vehicle_Step_Steer_10s.yawrate.Data) - 
 % greater for the road vehicle than the formula vehicle. In addition, we can see 
 % that the time taken for the overshoot to settle to the steady state yaw rate 
 % value is longer for the road vehicle than the formula vehicle. With the formula 
-% vehicle yaw rate overshoot only ~1.2% (1.1642%) compared to the road vehicle's 
-% ~13.7% (13.6520%) it is clear that the performance of the formula vehicle is 
-% greater. This is further reflected by the time taken for the vehicle response 
-% to reach its steady state condition being significantly greater for the road 
-% vehicle compared to the formula vehicle.
+% vehicle yaw rate overshoot only ~1.16% compared to the road vehicle's ~13.7% 
+% it is clear that the performance of the formula vehicle is greater. This is 
+% further reflected by the time taken for the vehicle response to reach its steady 
+% state condition being significantly greater for the road vehicle compared to 
+% the formula vehicle.
 %% Comparing Response Time
 % Steady state yaw rate values assumed as 8.90 for FV and 5.47 for RV (2dp)
 
@@ -191,3 +191,7 @@ response_times = [FV_step_response_time_2dp(1)-3, RV_step_response_time_2dp(2)-3
 bar(response_time_cats,response_times)
 ylabel("Yaw Rate Response Time (s)")
 title("Vehicle Step Steer Yaw Rate Response Time Comparison (Time from Input to Steady State)")
+%% 
+% The response of the formula vehicle is greater than that of the road vehicle, 
+% with steady state conditions achieved much faster and with significantly less 
+% overshoot.
