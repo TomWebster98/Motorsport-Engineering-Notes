c2d3ccc95e684989b1f5e0995e3fdb0f5a0f1c72
Tom Webster <tomwebster98@gmail.com>
2023-08-18 13:20:17 +0100

    Adds Vertical dynamics assignment

---
+++ Vehicle_Dynamics_Modelling_and_Simulation/MATLAB_Simulink_Models/Vertical_Dynamics_Assignment/DOF4_model_parameters.m
@@ -0,0 +1,7 @@
+%% Vehicle Parameters
+m = 850;  %Mass [kg]
+m_usf = 30;  %Front axle unsprung mass [kg]
+m_usr = 35;  %Rear axle unsprung mass [kg]
+L = 3.50;    %Wheelbase [m]
+a = 1.80;    %Sprung mass CoM front distance [m]
+k_tyre = 400000;  %Tyre vertical stiffness [N/m]
